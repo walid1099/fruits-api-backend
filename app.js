8573f941983e906fb704ff9a488cbd14968d83c0
walid1099 <walidk1099@gmail.com>
2026-05-06 15:14:56 +0100

    Message

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const app = express();
 const fruits = require("./routes/fruits")
 
 app.get('/', (req, res) => {
    res.send('Hello Fruity!');
 })
 
+app.use(cors)();
 app.use(express.json());
 app.use('/fruits', fruits);
 
