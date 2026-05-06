5c26b32b09afd7c5b88cf2c96d1d90d49cf967db
walid1099 <walidk1099@gmail.com>
2026-05-06 15:59:09 +0100

    final push

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const app = express();
 const fruits = require("./routes/fruits")
 
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
    res.send('Hello Fruity!');
 })
 
-app.use(cors)();
+app.use(cors());
 app.use(express.json());
 app.use('/fruits', fruits);
 
